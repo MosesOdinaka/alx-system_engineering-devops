4d43de4181e5513ac8d15fa3ee3e2a1fe4bf6366
MosesOdinaka <iriele.moses@gmail.com>
2023-08-01 16:50:33 -0700

    Expression that matches cases.

---
+++ 0x06-regular_expressions/1-repetition_token_0.rb
@@ -0,0 +1,12 @@
+#!/usr/bin/env ruby
+
+if ARGV.length != 1
+  puts "Usage: #($0) <string>"
+  exit
+end
+
+input = ARGV[0]
+pattern = /hbt{2,5}n/
+
+matches = input.scan(pattern)
+puts matches.join
