7fb0852cddf45b552fb736eacd1dcfd9284d7151
Moses Iriele <iriele.moes@gmail.com>
2023-09-03 18:52:25 +0100

    deleted dir

--- 0x06-regular_expressions/100-textme.rb
+++
@@ -1,6 +0,0 @@
-#!/usr/bin/env ruby
-input = ARGV[0]
-from = input.scan(/from:(\+?\w+)/).flatten.first
-to = input.scan(/to:(\+?\w+)/).flatten.first
-flags = input.scan(/flags:(\S+)/).flatten.first
-puts "#{from},#{to},#{flags}"
--- 0x06-regular_expressions/7-OMG_WHY_ARE_YOU_SHOUTING.rb
+++
@@ -1,3 +0,0 @@
-#!/usr/bin/env ruby
-
-puts ARGV[0].scan(/[A-Z]+/).join
