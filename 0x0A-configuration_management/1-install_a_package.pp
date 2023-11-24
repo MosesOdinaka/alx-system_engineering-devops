b8ceb34ec3ab4c6b015704ba40443f7e77c62645
MosesOdinaka <iriele.moses@gmail.com>
2023-11-25 00:28:44 +0100

    A Puppet manifest that installs Flask version 2.1.0 using pip3.

--- 0x0A-configuration_management/1-install_a_package.pp
+++ 0x0A-configuration_management/1-install_a_package.pp
@@ -1,6 +1,6 @@
 # A Puppet manifest that installs Flask version 2.1.0 using pip3.
 
-package { 'Flask':
-    ensure   => '2.1.0',
-    provider => 'pip3',
+exec { 'install_flask':
+    command => 'sudo pip3 install Flask==2.1.0',
+    path    => '/usr/bin:/usr/sbin:/bin',
 }
