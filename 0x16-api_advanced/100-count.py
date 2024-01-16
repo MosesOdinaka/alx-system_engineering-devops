66cd7942b72fddc02449810fbb5116f42734d682
MosesOdinaka <iriele.moses@gmail.com>
2024-01-16 16:59:30 +0100

    a recursive function that interacts with the Reddit API.

--- 0x16-api_advanced/100-count.py
+++ 0x16-api_advanced/100-count.py
@@ -9,7 +9,9 @@ the subreddit is invalid, it prints nothing.
 
 import requests
 
-def count_words(subreddit_name, keyword_list, next_page_token='', keyword_dict={}):
+
+def count_words(subreddit_name, keyword_list, next_page_token='',
+                keyword_dict={}):
     """
     Queries the Reddit API, parses the title of all hot articles, and prints
     a sorted count of given keywords. If the subreddit name is not a string
@@ -21,7 +23,8 @@ def count_words(subreddit_name, keyword_list, next_page_token='', keyword_dict={
                 keyword_dict[keyword.lower()] = 0
 
     if next_page_token is None:
-        sorted_keywords = sorted(keyword_dict.items(), key=lambda x: (-x[1], x[0]))
+        sorted_keywords = sorted(keyword_dict.items(), key=lambda x:
+                                 (-x[1], x[0]))
         for keyword in sorted_keywords:
             if keyword[1]:
                 print('{}: {}'.format(keyword[0], keyword[1]))
@@ -30,7 +33,8 @@ def count_words(subreddit_name, keyword_list, next_page_token='', keyword_dict={
     reddit_url = 'https://www.reddit.com/r/{}/hot/.json'.format(subreddit_name)
     request_headers = {'user-agent': 'redquery'}
     request_params = {'limit': 100, 'after': next_page_token}
-    response = requests.get(reddit_url, headers=request_headers, params=request_params,
+    response = requests.get(reddit_url, headers=request_headers,
+                            params=request_params,
                             allow_redirects=False)
 
     if response.status_code != 200:
@@ -41,7 +45,8 @@ def count_words(subreddit_name, keyword_list, next_page_token='', keyword_dict={
         next_page_token = response.json()['data']['after']
         for post in hot_posts:
             title = post['data']['title']
-            lower_case_title_words = [word.lower() for word in title.split(' ')]
+            lower_case_title_words = [word.lower() for word in
+                                      title.split(' ')]
 
             for keyword in keyword_dict.keys():
                 keyword_dict[keyword] += lower_case_title_words.count(keyword)
