b65f7942ddc524559ea9ddd90a361b6d8361d079
MosesOdinaka <iriele.moses@gmail.com>
2024-01-16 16:23:41 +0100

    fix bug

--- 0x16-api_advanced/0-subs.py
+++ 0x16-api_advanced/0-subs.py
@@ -1,16 +1,28 @@
 #!/usr/bin/python3
+"""
+This module contains a function that interacts with the Reddit API to fetch
+the number of subscribers for a specific subreddit.
+"""
 
-import requests as r
+import requests
 
-
-def number_of_subscribers(subreddit):
-    url = "https://www.reddit.com/r/{}/about.json".format(subreddit)
-    headers = {
-        "User-Agent": "Mozilla/5.0 (X11; Linux x86_64; rv:73.0) \
-        Gecko/20100101 Firefox/73.0"
-    }
-    response = r.get(url, headers=headers, allow_redirects=False)
-    if response.status_code == 404:
+def number_of_subscribers(subreddit_name):
+    """
+    Fetches and returns the number of subscribers for a specified subreddit.
+    If the subreddit name is not a string or is None, it returns 0.
+    """
+    if subreddit_name is None or not isinstance(subreddit_name, str):
         return 0
-    results = response.json().get("data")
-    return results.get("subscribers")
+
+    reddit_url = 'http://www.reddit.com/r/{}/about.json'.format(subreddit_name)
+    request_headers = {
+        'User-Agent': '0x16-api_advanced:project:v1.0.0 (by /u/Ajiboye Adeleye Pius)'
+    }
+
+    response = requests.get(reddit_url, headers=request_headers).json()
+
+    try:
+        subscriber_count = response.get('data').get('subscribers')
+        return subscriber_count
+    except Exception:
+        return 0
