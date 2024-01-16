7007396133272e2e68e9829fd851abe603f375c0
MosesOdinaka <iriele.moses@gmail.com>
2024-01-16 16:17:43 +0100

    fix bug

--- 0x16-api_advanced/0-subs.py
+++ 0x16-api_advanced/0-subs.py
@@ -1,22 +1,16 @@
 #!/usr/bin/python3
 
-import requests as req
+import requests as r
 
 
 def number_of_subscribers(subreddit):
-    """
-    This function returns the number of subscribers for a given subreddit.
-    If the subreddit does not exist, it returns 0.
-    """
-    reddit_url = "https://www.reddit.com/r/{}/about.json"
-    .format(subreddit)
-    headers_dict = {
+    url = "https://www.reddit.com/r/{}/about.json".format(subreddit)
+    headers = {
         "User-Agent": "Mozilla/5.0 (X11; Linux x86_64; rv:73.0) \
         Gecko/20100101 Firefox/73.0"
     }
-    response_obj = req.get(reddit_url, headers=headers_dict,
-                           allow_redirects=False)
-    if response_obj.status_code == 404:
+    response = r.get(url, headers=headers, allow_redirects=False)
+    if response.status_code == 404:
         return 0
-    subreddit_data = response_obj.json().get("data")
-    return subreddit_data.get("subscribers")
+    results = response.json().get("data")
+    return results.get("subscribers")
