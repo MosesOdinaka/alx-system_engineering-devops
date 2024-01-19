9da8032b68f42768e888d2d412137fa8556ddb80
MosesOdinaka <iriele.moses@gmail.com>
2024-01-19 15:26:16 +0100

    Puppet code that rectifies an issue in the WordPress settings file.

--- 0x17-web_stack_debugging_3/0-strace_is_your_friend.pp
+++ 0x17-web_stack_debugging_3/0-strace_is_your_friend.pp
@@ -1,14 +1,11 @@
-# This Puppet code rectifies an issue in the WordPress settings file.
+# This defined type rectifies an issue in the WordPress settings file by
+# replacing 'phpp' with 'php'.
 
-define rectify_wp_settings {
-    # The command to be executed.
-    $execute_command = 'sed -i s/phpp/php/g /var/www/html/wp-settings.php'
-    
-    # The path for the command execution.
-    $command_path = '/usr/local/bin/:/bin/'
-    
-    exec { 'rectify':
-        command => $execute_command,
-        path    => $command_path
-    }
+$file_to_edit = '/var/www/html/wp-settings.php'
+
+#replace line containing "phpp" with "php"
+
+exec { 'replace_line':
+  command => "sed -i 's/phpp/php/g' ${file_to_edit}",
+  path    => ['/bin','/usr/bin']
 }
